420d2b7b651329886fe1fdad21391c34f2238af9
Luis de la Cruz <gael130819@gmail.com>
2023-02-20 01:08:51 -0500

    refactor: remove no used methods

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -12,44 +12,15 @@ export class TodoItem extends LitElement {
     return [style];
   }
 
-  setStatusCompleted(e) {
-    const todoList = JSON.parse(localStorage.getItem('litTodoList')) || [];
-    const data = {
-      status: e.target.checked,
-    };
-
-    const list = todoList.find((item) => item.value === this.item.value);
-    for (const [key, value] of Object.entries(data)) {
-      list[key] = value;
-    }
-
-    console.log('list', list);
-    localStorage.setItem('litTodoList', JSON.stringify(todoList));
-  }
-
-  handleRemoveTodo() {
-    const todoList = JSON.parse(localStorage.getItem('litTodoList')) || [];
-    const list = todoList.findIndex((item) => item.value === this.item.value);
-    if (list >= 0) {
-      todoList.splice(list, 1);
-    }
-    localStorage.setItem('litTodoList', JSON.stringify(todoList));
+  attributeChangedCallback(name, oldVal, newVal) {
+    console.log('attribute change: ', name, newVal);
+    //super.attributeChangedCallback(name, oldVal, newVal);
   }
 
   render() {
     return html`
       <div class="todo-item">
         <span>${this.item.value}</span>
-        <div class="todo-actions">
-          <input
-            type="checkbox"
-            @change=${this.setStatusCompleted}
-            ?checked=${this.item.status}
-          />
-          <button color="red" @click="${this.handleRemoveTodo}">
-            Delete
-          </button>
-        </div>
       </div>
     `;
   }
